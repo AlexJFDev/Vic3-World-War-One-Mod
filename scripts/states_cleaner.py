21cb27e0d84711db37e47869b0c64b7e5069f3aa
alex <alexjfuhr@gmail.com>
2023-12-23 21:00:15 -0500

    Updated readers

--- scripts/states_cleaner.py
+++ scripts/states_cleaner.py
@@ -1,9 +1,8 @@
+from claus_reader import read_file
+
 FILE_PATH = 'small_states.txt'
 
 if __name__ == '__main__':
-    bracket_depth = 0
-    with open(FILE_PATH, 'rt') as file:
-        file_dict = {}
-        for line in file:
-            if '=' in line:
-                left_side, right_side = line.split('=')
+    claus_ob = read_file(FILE_PATH)
+    # print(claus_ob['OTHER'])
+    print(claus_ob)
