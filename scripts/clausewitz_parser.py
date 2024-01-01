3fbe9e4705f12d912494e26685d852fada96a725
alex <alexjfuhr@gmail.com>
2024-01-01 18:35:48 -0500

    Got homeland data

--- scripts/clausewitz_parser.py
+++ scripts/clausewitz_parser.py
@@ -6,8 +6,7 @@ import os
 import csv
 
 FILE_PATH = os.path.join('data', '00_states.txt')
-SAVE_PATH = os.path.join('data', 'new_states.txt')
-CSV_PATH = os.path.join('data', 'states.csv')
+CSV_PATH = os.path.join('data', 'homelands.csv')
 
 # There is a bug with one line objects
 
@@ -67,27 +66,15 @@ def parse_object(lines: list[str]) -> ClausewitzObject:
     return clausewitz_object
 
 if __name__ == '__main__':
-    
-    
     file_as_object: ClausewitzRoot = parse_file(FILE_PATH)
 
-    
-        
     with open(CSV_PATH, 'w') as file:
         csv_writer = csv.writer(file)
         states: dict[str, ClausewitzObject] = file_as_object.get_named_value('STATES').get_name_values()
 
         for state_name, state_data in states.items():
-            print(state_name)
-            owners: list[ClausewitzObject] = state_data[0].get_named_values("create_state")
-            for owner in owners:
-                tag: str = owner.get_named_value('country')
-                provinces: str = ' '.join(owner.get_named_value('owned_provinces').get_anonymous_values())
-                print(f'    {tag}: {provinces}')
-                csv_writer.writerow([state_name, tag, provinces])
-        
-        
-
-    unparsed = file_as_object.unparse(separator='    ')
-    with open(SAVE_PATH, 'w') as file:
-        file.write(unparsed)
+            try:
+                homelands = ' '.join(state_data[0].get_named_values('add_homeland'))
+                csv_writer.writerow([state_name, homelands])
+            except KeyError:
+                csv_writer.writerow([state_name, ''])
