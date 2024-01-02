c7e569fc989fc1db12e6b7fd78a17a785f5c617f
alex <alexjfuhr@gmail.com>
2024-01-02 11:47:58 -0500

    Added homelands to generated states

--- scripts/states_generator.py
+++ scripts/states_generator.py
@@ -13,7 +13,7 @@ OWNERSHIP_REGION_TAG_COLUMN = 2
 OWNERSHIP_COUNTRY_TAG_COLUMN = 3
 OWNERSHIP_PROVINCES_COLUMN = 4
 REGIONS_STATE_TAG_COLUMN = 1
-REGIONS_HOMLANDS_COLUMN = 2
+REGIONS_HOMELANDS_COLUMN = 2
 
 root = ClausewitzRoot()
 root.add_named_value('STATES', ClausewitzObject())
@@ -35,6 +35,16 @@ with open(OWNERSHIP_FILE, 'r') as file:
             'owned_provinces': [ClausewitzObject(anonymous_values=provinces.split(' '))],
             'country': [owner_tag]
         }))
+
+with open(REGIONS_FILE, 'r') as file:
+    file.readline() # Skip the first line
+    reader = csv.reader(file)
+    for line in reader:
+        region_tag: str = line[REGIONS_STATE_TAG_COLUMN]
+        homelands = line[REGIONS_HOMELANDS_COLUMN].split(' ')
+        state: ClausewitzObject = states.get_named_value(region_tag)
+        for homeland in homelands:
+            state.add_named_value('add_homeland', homeland)
 
 
 with open(OUTPUT_FILE, 'w') as file:
