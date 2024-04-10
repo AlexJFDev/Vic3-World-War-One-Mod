cbe70d739d88cf2a3c2f2d83fce984a6a4c8aa4c
alex <alexjfuhr@gmail.com>
2024-04-10 00:14:01 -0400

    Added documentation and a method

--- scripts/clausewitz_object.py
+++ scripts/clausewitz_object.py
@@ -1,5 +1,9 @@
 class ClausewitzObject:
+    """An object used for working with Paradox modding files.
+    Fundamentally, it is like a dictionary and a list. It allows for 'named values', which are stored in a dictionary and 'anonymous values' which are stored in a list.
+    Furthermore, any number of values can be associated with a name instead of just one like a regular dictionary."""
     def __init__(self, name_values:'dict[str, list]|None'=None, anonymous_values:'list|None'=None) -> None:
+        """Initialize a new ClausewitzObject. Optionally takes a dictionary in 'name_values' or a list in 'anonymous_values'."""
         self.name_values = name_values
         self.anonymous_values = anonymous_values
         if name_values is None:
@@ -8,11 +12,13 @@ class ClausewitzObject:
             self.anonymous_values: list = []
     
     def add_named_value(self, name: str, value) -> None:
+        """Add a named value. Takes a string for the name and a value."""
         previous_values = self.name_values.get(name, [])
         previous_values.append(value)
         self.name_values[name] = previous_values
 
     def get_named_value(self, name: str, index=0, default=None) -> 'str|int|ClausewitzObject|None':
+        """Gets a single value assigned to a name. Defaults to index 0 and None if no values are assigned to the name."""
         values = self.name_values.get(name, None)
         if values is None:
             return default
@@ -21,32 +27,44 @@ class ClausewitzObject:
         return values[index]
     
     def get_named_values(self, name: str, default=None) -> list:
+        """Gets all values assigned to a name in the form of a list. Returns None if no values are assigned to the name."""
         values = self.name_values.get(name, None)
         if values is None:
             return default
         return values
     
     def get_name_values(self) -> dict[str, list]:
+        """Gets all names and their values in the form of a dictionary with names as the keys and lists of values as the values."""
         return self.name_values
+    
+    def get_names(self) -> list:
+        """Returns a list containing all names in the object."""
+        return self.name_values.keys()
 
     def add_anonymous_value(self, value) -> None:
+        """Adds an anonymous value to the object."""
         self.anonymous_values.append(value)
 
     def add_anonymous_values(self, *values) -> None:
+        """Adds any number of anonymous values to the object."""
         self.anonymous_values += values
 
     def join_anonymous_values(self, values) -> None:
+        """Joins a list of anonymous values to the preexisting anonymous values."""
         self.anonymous_values += values
     
     def get_anonymous_values(self) -> list:
+        """Gets the entire list of anonymous values."""
         return self.anonymous_values
     
     def pop_anonymous_values(self):
+        """Pops the last anonymous value."""
         if len(self.anonymous_values == 0):
             raise IndexError('Cannot pop object with no anonymous values.')
         return self.anonymous_values.pop()
     
     def unparse(self, depth=0, separator='\t') -> str:
+        """Generates a string, representing the object, that can be used in a Paradox mod."""
         full_separator: str = separator * depth
 
         anonymous_objects: str = ''
