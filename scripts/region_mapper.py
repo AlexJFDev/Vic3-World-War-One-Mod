21d65dcda4454e9026759bd9354c8a3eb30f9fdd
alex <alexjfuhr@gmail.com>
2024-07-19 17:32:56 -0400

    Improved mapper

--- scripts/region_mapper.py
+++ scripts/region_mapper.py
@@ -6,28 +6,27 @@ import random
 
 PROVINCE_DATA: str = os.path.join('game', 'data', 'state_regions.csv')
 INPUT_MAP: str = os.path.join('game', 'files', 'map_data', 'provinces.png')
-OUTPUT_MAP: str = os.path.join('game', 'data', 'state_regions.png')
+OUTPUT_MAP: str = os.path.join('game', 'data', 'state_regions2.png')
 
 def random_color():
     return np.array([random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)])
 
-def convert_colors(provinces):
-    province_ids = provinces.split(' ')
+def convert_colors(provinces: list[str]) -> list[np.ndarray]:
     colors = []
-    for province in province_ids:
+    for province in provinces:
         #print(province)
         red = int(province[1:3], 16)
         blue = int(province[3:5], 16)
         green = int(province[5:7], 16)
 
-        colors.append([red, blue, green])
+        colors.append(np.array([red, blue, green]))
     return colors
 
 if __name__ == '__main__':
     map = Image.open(INPUT_MAP)
     #map.show()
 
-    np_map = np.array(map)
+    map_array = np.array(map)
     #print(np_map)
 
     print('Generating color map')
@@ -35,22 +34,26 @@ if __name__ == '__main__':
     with open(PROVINCE_DATA, 'r') as province_file:
         province_reader = csv.reader(province_file)
         for line in province_reader:
-            provinces = line[9]
+            province_ids = line[9].split(' ')
+            province_colors = convert_colors(province_ids)
+            if len(province_ids) == 1: # Is ocean
+                state_color = np.array([255, 255, 255])
+            else: # Is land
+                state_color = province_colors[0]
             #print(provinces)
-            colors = convert_colors(provinces)
-            state_color = random_color()
-            for color in colors:
-                color_map[str(np.array(color))] = state_color
+            for province_color in province_colors:
+                color_map[str(province_color)] = state_color
     print('Generating new map')
-    for row_num, row in enumerate(np_map):
+    for row_num, row in enumerate(map_array):
         print(f'    Row: {row_num}')
+        if row_num > 19: break
         for col_num, col in enumerate(row):
             col_string = str(col)
             new_color = color_map.get(col_string)
             if new_color is not None:
-                np_map[row_num][col_num] = new_color
+                map_array[row_num][col_num] = new_color
     
-    new_map = Image.fromarray(np_map)
+    new_map = Image.fromarray(map_array)
     new_map.show()
     new_map.save(OUTPUT_MAP)
     
