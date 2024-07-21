8d90ba4e2cf0b9a24f8efd3093887d10017dec01
alex <alexjfuhr@gmail.com>
2024-07-21 13:49:32 -0400

    Added new_states data

--- scripts/states_reader.py
+++ scripts/states_reader.py
@@ -5,7 +5,7 @@ import clausewitz_parser
 from clausewitz_object import ClausewitzObject
 from clausewitz_root import ClausewitzRoot
 
-NEW_STATES_PATH = os.path.join('game', 'files', 'new_states.txt')
+NEW_STATES_PATH = os.path.join('game', 'files', 'common', 'history', 'states', '00_states.txt')
 MOD_STATES_PATH = os.path.join('mod', 'files', 'states.txt')
 NEW_STATES_CSV = os.path.join('game', 'data', 'new_states.csv')
 
@@ -13,7 +13,7 @@ OWNERSHIP_REGION_TAG_COLUMN = 0
 OWNERSHIP_COUNTRY_TAG_COLUMN = 3
 OWNERSHIP_PROVINCES_COLUMN = 4
 
-def unparse_states(states_root: ClausewitzRoot):
+def convert_states(states_root: ClausewitzRoot):
     states = {}
     state_names = states_root.get_value_named('STATES').get_names()
     for state_name in state_names:
@@ -36,5 +36,5 @@ def save_states(states: dict[str: list[str]]):
 if __name__ == '__main__':
     with open(NEW_STATES_PATH, 'rt') as states_file:
         new_states_root = clausewitz_parser.parse_file(states_file)
-    new_states = unparse_states(new_states_root)
+    new_states = convert_states(new_states_root)
     save_states(new_states)
