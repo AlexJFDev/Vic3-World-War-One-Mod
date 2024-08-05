77afa902b5fde1d9708cbfb536998e8b06b7e789
alex <alexjfuhr@gmail.com>
2024-08-05 00:23:00 -0400

    Updated state regions

--- scripts/states_generator.py
+++ scripts/states_generator.py
@@ -5,17 +5,34 @@ import os
 import csv
 
 OWNERSHIP_FILE: str = os.path.join('mod', 'data', 'state_ownership.csv')
-REGIONS_FILE: str = os.path.join('mod', 'data', 'state_regions.csv')
+REGION_DATA_FILE: str = os.path.join('mod', 'data', 'state_regions.csv')
 
 STATES_FILE: str = os.path.join('mod', 'files', 'states.txt')
 POPS_FILE: str = os.path.join('mod', 'files', 'pops.txt')
+REGIONS_FILE: str = os.path.join('mod', 'files', 'state_regions.txt')
 
 OWNERSHIP_REGION_TAG_COLUMN = 0
 OWNERSHIP_COUNTRY_TAG_COLUMN = 3
 OWNERSHIP_PROVINCES_COLUMN = 4
 OWNERSHIP_POPS_COLUMN = 5
-REGIONS_STATE_TAG_COLUMN = 1
+
+REGIONS_REGION_TAG_COLUMN = 0
+REGIONS_ID_COLUMN = 1
 REGIONS_HOMELANDS_COLUMN = 2
+REGIONS_SUBSISTENCE_BUILDING_COLUMN = 3
+REGIONS_CITY_COLUMN = 4
+REGIONS_PORT_COLUMN = 5
+REGIONS_FARM_COLUMN = 6
+REGIONS_MINE_COLUMN = 7
+REGIONS_WOOD_COLUMN = 8
+REGIONS_ARABLE_LAND_COLUMN = 9
+REGIONS_PROVINCES_COLUMN = 10
+REGIONS_TRAITS_COLUMN = 11
+REGIONS_ARABLE_RESOURCES_COLUMN = 12
+REGIONS_CAPPED_RESOURCES_COLUMN = 13
+REGIONS_NAVAL_EXIT_COLUMN = 14
+
+ENCODING = 'utf-8-sig'
 
 def split_pops(pops: str) -> list[str]:
     groups = pops.split(',')
@@ -80,22 +97,82 @@ def generate_states_and_pops(ownership_path: str, regions_path: str):
         regions_file.readline() # Skip the first line
         reader = csv.reader(regions_file)
         for line in reader:
-            region_tag: str = line[REGIONS_STATE_TAG_COLUMN]
+            region_tag: str = f's:{line[REGIONS_REGION_TAG_COLUMN]}'
             homelands = line[REGIONS_HOMELANDS_COLUMN].split(' ')
-            state: ClausewitzObject = states.get_value_named(region_tag)
-                
+            state = states.get_value_named(region_tag)
+            if state is None:
+                continue
             for homeland in homelands:
                 if homeland == '':
                     continue
                 state.add_named_value('add_homeland', homeland)
     return states_root, pops_root
 
+def generate_regions(regions_path: str):
+    regions_root = ClausewitzRoot()
+    with open(regions_path, 'r') as regions_file:
+        regions_file.readline() # Skip the first line
+
+        reader = csv.reader(regions_file)
+        for line_num, line in enumerate(reader, 2):
+            region_tag: str = line[REGIONS_REGION_TAG_COLUMN]
+            region_id = line[REGIONS_ID_COLUMN]
+            subsistence_building_type = line[REGIONS_SUBSISTENCE_BUILDING_COLUMN]
+            city_province = line[REGIONS_CITY_COLUMN]
+            port_province = line[REGIONS_PORT_COLUMN]
+            farm_province = line[REGIONS_FARM_COLUMN]
+            mine_province = line[REGIONS_MINE_COLUMN]
+            wood_province = line[REGIONS_WOOD_COLUMN]
+            arable_land = line[REGIONS_ARABLE_LAND_COLUMN]
+            provinces = line[REGIONS_PROVINCES_COLUMN].split(' ')
+            traits = line[REGIONS_TRAITS_COLUMN].split(' ')
+            arable_resources = line[REGIONS_ARABLE_RESOURCES_COLUMN].split(' ')
+            capped_resources = line[REGIONS_CAPPED_RESOURCES_COLUMN].split(' ')
+            naval_exit = line[REGIONS_NAVAL_EXIT_COLUMN]
+
+            region_object = ClausewitzObject()
+            region_object.add_named_value('id', region_id)
+            region_object.add_named_value('provinces', ClausewitzObject(anonymous_values=provinces))
+            if (subsistence_building_type != ''):
+                region_object.add_named_value('subsistence_building', subsistence_building_type)
+            if (city_province != ''):
+                region_object.add_named_value('city', city_province)
+            if (port_province != ''):
+                region_object.add_named_value('port', port_province)
+            if (farm_province != ''):
+                region_object.add_named_value('farm', farm_province)
+            if (mine_province != ''):
+                region_object.add_named_value('mine', mine_province)
+            if (wood_province != ''):
+                region_object.add_named_value('wood', wood_province)
+            if (arable_land != ''):
+                region_object.add_named_value('arable_land', arable_land)
+            if (traits[0] != ''):
+                region_object.add_named_value('traits', ClausewitzObject(anonymous_values=traits))
+            if (arable_resources[0] != ''):
+                region_object.add_named_value('arable_resources', ClausewitzObject(anonymous_values=arable_resources))
+            if (capped_resources[0] != ''):
+                capped_resources_object = ClausewitzObject()
+                for capped_resource in capped_resources:
+                    resource, quantity = capped_resource.split('=')
+                    capped_resources_object.add_named_value(resource, quantity)
+                region_object.add_named_value('capped_resources', capped_resources_object)
+            if (naval_exit != ''):
+                region_object.add_named_value('naval_exit', naval_exit)
+
+            regions_root.add_named_value(region_tag, region_object)
+    return regions_root
+
 if __name__ == '__main__':
-    states_root, pops_root = generate_states_and_pops(OWNERSHIP_FILE, REGIONS_FILE)
-    with open(STATES_FILE, 'w') as file:
+    states_root, pops_root = generate_states_and_pops(OWNERSHIP_FILE, REGION_DATA_FILE)
+    regions_root = generate_regions(REGION_DATA_FILE)
+    with open(STATES_FILE, 'w', encoding=ENCODING) as file:
         file.write(states_root.unparse())
 
-    with open(POPS_FILE, 'w') as file:
+    with open(POPS_FILE, 'w', encoding=ENCODING) as file:
         file.write(pops_root.unparse())
 
+    with open(REGIONS_FILE, 'w', encoding=ENCODING) as file:
+        file.write(regions_root.unparse())
+    
     print('Success!')
