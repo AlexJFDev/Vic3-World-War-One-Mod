27e3afd9edab92c0b22bcf1c8d3fa773c49f81d1
alex <alexjfuhr@gmail.com>
2024-12-20 19:02:17 -0500

    Updated script

--- scripts/regions_updater.py
+++ scripts/regions_updater.py
@@ -1,50 +1,67 @@
+# This script is for updating state regions after a game update
 import os
 import csv
 
-OLD_REGIONS_DATA_PATH = os.path.join('mod', 'data', 'old_state_regions.csv')
-NEW_REGIONS_DATA_PATH = os.path.join('game', 'data', 'state_regions.csv')
-REGIONS_DATA_PATH = os.path.join('mod', 'data', 'state_regions.csv')
+# The format of the file
+# The file path
 
-OLD_REGION_NAME_COLUMN = 0
-OLD_REGION_TAG_COLUMN = 1
-OLD_REGION_HOMELANDS_COLUMN = 2
+# Region Tag,ID Number,Homelands,Subsistence Building,City,Port,Farm,Mine,Wood,Arable Land,Provinces,Traits,Arable Resources,Caped Resources,Naval Exit
+OLD_MOD_REGIONS_DATA_PATH = os.path.join('mod', 'data', 'old_state_regions.csv')
+# Region Tag,ID Number,Subsistence Building,City,Port,Farm,Mine,Wood,Arable Land,Provinces,Traits,Arable Resources,Caped Resources,Naval Exit,Prime Land
+NEW_GAME_REGIONS_DATA_PATH = os.path.join('game', 'data', 'state_regions.csv')
+# Region Tag,ID Number,Homelands,Subsistence Building,City,Port,Farm,Mine,Wood,Arable Land,Provinces,Traits,Arable Resources,Caped Resources,Naval Exit, Prime Land
+NEW_MOD_REGIONS_DATA_PATH = os.path.join('mod', 'data', 'state_regions.csv')
 
-NEW_REGIONS_TAG_COLUMN = 0
-NEW_REGIONS_ID_COLUMN = 1
-NEW_REGIONS_PROVINCES_COLUMN = 9
+MOD_REGION_TAG_COLUMN = 0
+MOD_REGION_ID_COLUMN = 1
+MOD_REGION_HOMELANDS_COLUMN = 2
+MOD_REGION_SUBSISTENCE_BUILDING_COLUMN = 3
+MOD_REGION_CITY_COLUMN = 4
+MOD_REGION_PORT_COLUMN = 5
+MOD_REGION_FARM_COLUMN = 6
+MOD_REGION_MINE_COLUMN = 7
+MOD_REGION_WOOD_COLUMN = 8
+MOD_REGION_ARABLE_LAND_COLUMN = 9
+MOD_REGION_PROVINCES_COLUMN = 10
+MOD_REGION_TRAITS_COLUMN = 11
+MOD_REGION_ARABLE_RESOURCES_COLUMN = 12
+MOD_REGION_CAPED_RESOURCES_COLUMN = 13
+MOD_REGION_NAVAL_EXIT_COLUMN = 14
+MOD_REGION_PRIME_LAND_COLUMN = 15
 
-old_regions = {}
-with open(OLD_REGIONS_DATA_PATH, 'r') as file:
+GAME_REGION_TAG_COLUMN = 0
+GAME_REGION_ID_COLUMN = 1
+GAME_REGION_SUBSISTENCE_BUILDING_COLUMN = 2
+GAME_REGION_CITY_COLUMN = 3
+GAME_REGION_PORT_COLUMN = 4
+GAME_REGION_FARM_COLUMN = 5
+GAME_REGION_MINE_COLUMN = 6
+GAME_REGION_WOOD_COLUMN = 7
+GAME_REGION_ARABLE_LAND_COLUMN = 8
+GAME_REGION_PROVINCES_COLUMN = 9
+GAME_REGION_TRAITS_COLUMN = 10
+GAME_REGION_ARABLE_RESOURCES_COLUMN = 11
+GAME_REGION_CAPED_RESOURCES_COLUMN = 12
+GAME_REGION_NAVAL_EXIT_COLUMN = 13
+GAME_REGION_PRIME_LAND_COLUMN = 14
+
+# First iterate through the old mod regions
+old_mod_regions = {}
+with open(OLD_MOD_REGIONS_DATA_PATH, 'r') as file:
+    file.readline() # Skip first line
     reader = csv.reader(file)
-    for row in reader:
-        name = row[OLD_REGION_NAME_COLUMN]
-        tag = row[OLD_REGION_TAG_COLUMN]
-        homelands = row[OLD_REGION_HOMELANDS_COLUMN]
-        old_regions[tag] = (name, homelands)
+    for line in reader:
+        pass
 
-new_regions = {}
-with open(NEW_REGIONS_DATA_PATH, 'r') as file:
+# Then iterate through the new game regions
+new_game_regions = {}
+with open(NEW_GAME_REGIONS_DATA_PATH, 'r') as file:
+    file.readline() # Skip first line
     reader = csv.reader(file)
-    for row in reader:
-        tag = f's:{row[NEW_REGIONS_TAG_COLUMN]}'
-        id = int(row[NEW_REGIONS_ID_COLUMN])
-        provinces = row[NEW_REGIONS_PROVINCES_COLUMN]
-        if id < 3000: # Ocean region
-            new_regions[tag] = provinces
+    for line in reader:
+        pass
 
-lines = []
-for tag, provinces in new_regions.items():
-    if tag in old_regions:
-        old_name, old_homelands = old_regions[tag]
-        lines.append([old_name, tag, old_homelands, provinces,'', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', ''])
-    else:
-        lines.append(['', tag, '', provinces,'', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', ''])
-
-lines.sort(key=lambda line: line[1])
-
-lines.insert(0, ['State Name', 'State Region Tag', 'Homelands', 'Provinces', 'Arable Land', 'Traits', 'Claims', 'Strategic Region', 'File Region', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', ''])
-
-with open(REGIONS_DATA_PATH, 'w', newline='') as regions_file:
-    writer = csv.writer(regions_file, quoting=csv.QUOTE_ALL)
-    for line in lines:
-        writer.writerow(line)
+# with open(NEW_MOD_REGIONS_DATA_PATH, 'w', newline='') as regions_file:
+#     writer = csv.writer(regions_file, quoting=csv.QUOTE_ALL)
+#     for line in lines:
+#         writer.writerow(line)
